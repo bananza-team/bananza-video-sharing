b3636ee49638998e3e6670fef01938b7ea81c0eb
TCLaurentiu <claudiu.tirisi@student.upt.ro>
2022-04-23 20:26:28 +0300

    BNNZ-38 Add structure for the guest view of the Homepage

--- bananza-frontend/pages/index.js
+++ bananza-frontend/pages/index.js
@@ -1,6 +1,15 @@
 import PageHead from "/components/general/pageHead.js"
+import "../styles/Home.module.css"
+import Image from 'next/image'
 export default function Home() {
   return (
-    <PageHead pageTitle="test"></PageHead>
+    <>
+    <PageHead pageTitle="Bananza - Homepage"></PageHead>
+    <div className="big-logo"><img src="/logo.png"/></div>
+      <div className="guest-options">
+        <span className="guest-login">Login</span>
+        <span className="guest-register">Register</span>
+      </div>
+    </>
   )
 }
