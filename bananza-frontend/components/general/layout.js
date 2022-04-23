cf6b05f6325bcc552a0245f4c1f913bfbdbf0499
TCLaurentiu <claudiu.tirisi@student.upt.ro>
2022-04-23 19:54:50 +0300

    BNNZ-38 Move page title from Layout component to the pages

--- bananza-frontend/components/general/layout.js
+++ bananza-frontend/components/general/layout.js
@@ -3,7 +3,6 @@ import PageHead from "/components/general/pageHead.js"
 export default function layout({children}, props){
     return (
         <>
-        <PageHead pageTitle={props.pageTitle}/>
         {children}
         </>
     )
