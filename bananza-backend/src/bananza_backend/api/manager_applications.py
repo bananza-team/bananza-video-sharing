68ae2ead51b44d161824b02a9867cad4a4e48a21
serbanlex <serbanlex2001@yahoo.com>
2022-04-29 01:29:45 +0300

    BNNZ-152 Create GET /application/{application_id} endpoint that routes to get_by_id repo logic

--- bananza-backend/src/bananza_backend/api/manager_applications.py
+++ bananza-backend/src/bananza_backend/api/manager_applications.py
@@ -5,11 +5,18 @@ from bananza_backend.services.manager_applications.repository import ManagerAppl
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-router = APIRouter(prefix="/applications", tags=["Manager applications"])
+router = APIRouter(prefix="/application", tags=["Manager applications"])
 
 
 @router.post("/{user_id}", summary="Register a user to the db, by his id", response_model=ManagerApplication)
 async def submit_user_application(user_id: str, db: Session = Depends(get_db)):
     applications_repo = ManagerApplicationsRepo(db)
     new_application = applications_repo.add(user_id=user_id)
+    return new_application
+
+
+@router.get("/{application_id}", summary="Get a user application from the db", response_model=ManagerApplication)
+async def get_user_application(application_id: str, db: Session = Depends(get_db)):
+    applications_repo = ManagerApplicationsRepo(db)
+    new_application = applications_repo.get_by_id(application_id=application_id)
     return new_application
