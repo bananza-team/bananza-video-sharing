b8fad535af477cf55e524abc46f8ad44c68cd175
TCLaurentiu <claudiu.tirisi@student.upt.ro>
2022-04-29 20:01:32 +0300

    BNNZ-39 Add onChange property to the input component

--- bananza-frontend/components/forms/input.js
+++ bananza-frontend/components/forms/input.js
@@ -1,13 +1,22 @@
+import React, { useState } from "react";
 export default function input(props) {
+  let defaultChange=(e)=>{
+    setValue(e.target.value);
+  }
+  let [value, setValue]=useState(props.value);
   return (
     <span>
-      <label htmlFor={props.name}>{props.label}
-      <input
-        name={props.name}
-        id={props.name}
-        type={props.inputType}
-        placeholder={props.placeholderText}
-      ></input>
+      <label htmlFor={props.name}>
+        {props.label}
+        <input
+          name={props.name}
+          id={props.name}
+          type={props.inputType}
+          placeholder={props.placeholderText}
+          value={value}
+          className={props.className}
+          onChange={defaultChange}
+        ></input>
       </label>
     </span>
   );
