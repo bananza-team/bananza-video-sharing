22268fa9061effccebfd7a9f299aa2ed7791a7a8
TCLaurentiu <claudiu.tirisi@student.upt.ro>
2022-05-09 20:41:04 +0300

    BNNZ-48 Add a loading component that is displayed while the user data is loaded in

--- bananza-frontend/libs/users/currentuser.js
+++ bananza-frontend/libs/users/currentuser.js
@@ -2,23 +2,28 @@ import React, { useState, useEffect } from "react";
 
 export default function getCurrentUser() {
   let [user, setUser] = useState(null);
+  let [loading, setLoading] = useState(1);
 
   useEffect(() => {
     let token = localStorage.token;
     ((token) => {
       fetch("//localhost:8000/user/current", {
         headers: {
-          "Authorization": "Bearer "+token,
+          Authorization: "Bearer " + token,
         },
       }).then((response) =>
         response.json().then((parsedJSON) => {
           if (response.status == 200) {
             setUser(parsedJSON);
           }
+          setLoading(0);
         })
       );
     })(token);
   }, []);
 
-  return user;
+  return {
+    user: user,
+    loading: loading,
+  };
 }
