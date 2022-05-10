597dfa30a3db3f4b1a3e96c1d150406767d58521
serbanlex <serbanlex2001@yahoo.com>
2022-05-10 23:55:45 +0300

    BNNZ-68 Create the UserPublic model for essential data that will be shown on profiles

--- bananza-backend/src/bananza_backend/models.py
+++ bananza-backend/src/bananza_backend/models.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel, Field
 from enum import Enum
 from typing import List, Optional
-
+from typing import Any
 
 class UserTypeEnum(str, Enum):
     creator = "creator"
@@ -9,14 +9,18 @@ class UserTypeEnum(str, Enum):
     admin = "admin"
 
 
-class UserBase(BaseModel):
+class UserPublic(BaseModel):
     username: Optional[str] = Field(description="Username, introduced at register")
-    email: Optional[str] = Field(description="Email, introduced at register")
     type: Optional[UserTypeEnum] = Field(description="Type of user, deciding his rights (creator, manager, admin)")
     description: Optional[str] = Field(description="Description which appears on the user's profile")
     profile_picture_link: Optional[str] = Field(description="Profile pic which appears on the user's profile")
     cover_picture_link: Optional[str] = Field(description="Cover picture which appears on the user's profile")
-    is_active: Optional[bool] = Field(description="User's active status, False if he's suspended/he deleted his account")
+    is_active: Optional[bool] = Field(
+        description="User's active status, False if he's suspended/he deleted his account")
+
+
+class UserBase(UserPublic):
+    email: Optional[str] = Field(description="Email, introduced at register")
     cv_link: Optional[str] = Field(description="CV link, existent only if user applies for management position",
                                    default="")
     name: Optional[str] = Field(description="Name, existent only if user applies for management position", default="")
