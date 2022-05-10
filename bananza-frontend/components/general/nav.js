00353b98962b7cbd0acfd898b9bace72dfa7bcb3
TCLaurentiu <claudiu.tirisi@student.upt.ro>
2022-05-10 21:43:06 +0300

    BNNZ-48 Add an element inbetween the links and the navbar

--- bananza-frontend/components/general/nav.js
+++ bananza-frontend/components/general/nav.js
@@ -1,29 +1,31 @@
 import Link from "next/link";
 import Router from "next/router";
 
-let logout = (event) =>{
+let logout = (event) => {
   event.preventDefault();
   delete localStorage.token;
   Router.reload();
-}
+};
 
 export default function Nav() {
   return (
     <nav>
-      <span className="navLogo">
-        <img src="/logo.png" />
-      </span>
-      <span className="leftNav">
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-        <Link href="/profile">
-          <a>Profile</a>
-        </Link>
-      </span>
-      <span className="rightNav">
+      <div className="nav-wrap">
+        <span className="navLogo">
+          <img src="/logo.png" />
+        </span>
+        <span className="leftNav">
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/profile">
+            <a>Profile</a>
+          </Link>
+        </span>
+        <span className="rightNav">
           <a onClick={logout}>Logout</a>
-      </span>
+        </span>
+      </div>
     </nav>
   );
 }
