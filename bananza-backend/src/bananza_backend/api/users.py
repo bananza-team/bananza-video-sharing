1734ef9522224ec3e6f06db35adda24e514c5ede
serbanlex <serbanlex2001@yahoo.com>
2022-05-10 23:55:00 +0300

    BNNZ-68 Modify GET /user/id to use a public User model to not expose unintended info

--- bananza-backend/src/bananza_backend/api/users.py
+++ bananza-backend/src/bananza_backend/api/users.py
@@ -1,4 +1,4 @@
-from bananza_backend.models import UserCreate, User
+from bananza_backend.models import UserCreate, User, UserPublic
 from bananza_backend.db.sql_db import get_db
 from bananza_backend.services.users.repository import UserRepo
 from bananza_backend.services.authentication.handler import AuthHandler, oauth2_scheme
@@ -16,10 +16,10 @@ async def register_user(user: UserCreate, db: Session = Depends(get_db)):
     return new_user
 
 
-@router.get("/{user_id}", summary="Get user by ID", response_model=User)
+@router.get("/{user_id}", summary="Get user by ID", response_model=UserPublic)
 async def get_user_by_id(id: int, db: Session = Depends(get_db)):
     user_repo = UserRepo(db)
-    return user_repo.get_by_id(id)
+    return user_repo.get_public_by_id(id)
 
 
 @router.get("/current", summary="Get current user based on JWT token", response_model=User)
@@ -28,3 +28,8 @@ async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = De
     user = auth_handler.get_current_user_by_token(token)
     return user
 
+
+@router.patch("/current/info", summary="Edit user by ID", response_model=User)
+async def edit_user_by_id(id: int, new_details: UserCreate, db: Session = Depends(get_db)):
+    user_repo = UserRepo(db)
+    return user_repo.edit_by_id(id, new_details)
