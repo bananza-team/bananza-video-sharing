5d4c345015dfdf25a899080fd7219247de1e0428
serbanlex <serbanlex2001@yahoo.com>
2022-05-11 12:32:15 +0300

    BNNZ-68 Change endpoint order in api/users.py

--- bananza-backend/src/bananza_backend/api/users.py
+++ bananza-backend/src/bananza_backend/api/users.py
@@ -1,4 +1,4 @@
-from bananza_backend.models import UserCreate, User, UserPublic
+from bananza_backend.models import UserCreate, User, UserPublic, UserEdit
 from bananza_backend.db.sql_db import get_db
 from bananza_backend.services.users.repository import UserRepo
 from bananza_backend.services.authentication.handler import AuthHandler, oauth2_scheme
@@ -16,12 +16,6 @@ async def register_user(user: UserCreate, db: Session = Depends(get_db)):
     return new_user
 
 
-@router.get("/{user_id}", summary="Get user by ID", response_model=UserPublic)
-async def get_user_by_id(id: int, db: Session = Depends(get_db)):
-    user_repo = UserRepo(db)
-    return user_repo.get_public_by_id(id)
-
-
 @router.get("/current", summary="Get current user based on JWT token", response_model=User)
 async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     auth_handler = AuthHandler(db)
@@ -29,7 +23,13 @@ async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = De
     return user
 
 
+@router.get("/{user_id}", summary="Get user by ID", response_model=UserPublic)
+async def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
+    user_repo = UserRepo(db)
+    return user_repo.get_public_by_id(user_id)
+
+
 @router.patch("/current/info", summary="Edit user by ID", response_model=User)
-async def edit_user_by_id(id: int, new_details: UserCreate, db: Session = Depends(get_db)):
+async def edit_user_by_id(id: int, new_details: UserEdit, db: Session = Depends(get_db)):
     user_repo = UserRepo(db)
     return user_repo.edit_by_id(id, new_details)
