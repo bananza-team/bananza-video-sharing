268b2a7aded079a5efe13eff3e11fb9a3fd7f3a6
serbanlex <serbanlex2001@yahoo.com>
2022-05-14 00:06:04 +0300

    BNNZ-54 Add PATCH endpoint for editing the current user's info

--- bananza-backend/src/bananza_backend/api/users.py
+++ bananza-backend/src/bananza_backend/api/users.py
@@ -11,25 +11,22 @@ router = APIRouter(prefix="/user", tags=["User management"])
 
 @router.post("/", summary="Register a user to the db", response_model=User)
 async def register_user(user: UserCreate, db: Session = Depends(get_db)):
-    user_repo = UserRepo(db)
-    new_user = user_repo.add(user)
+    new_user = UserRepo(db).add(user)
     return new_user
 
 
 @router.get("/current", summary="Get current user based on JWT token", response_model=User)
 async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-    auth_handler = AuthHandler(db)
-    user = auth_handler.get_current_user_by_token(token)
+    user = AuthHandler(db).get_current_user_by_token(token)
     return user
 
 
 @router.get("/{user_id}", summary="Get user by ID", response_model=UserPublic)
 async def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
-    user_repo = UserRepo(db)
-    return user_repo.get_public_by_id(user_id)
+    return UserRepo(db).get_public_by_id(user_id)
 
 
 @router.patch("/current/info", summary="Edit user by ID", response_model=User)
-async def edit_user_by_id(user_id: int, new_details: UserEdit, db: Session = Depends(get_db)):
-    user_repo = UserRepo(db)
-    return user_repo.edit_by_id(user_id, new_details)
+async def edit_current_user(new_details: UserEdit, db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+    user = AuthHandler(db).get_current_user_by_token(token)
+    return UserRepo(db).edit(user, new_details)
