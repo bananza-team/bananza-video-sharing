282fdb1c829f664dee4778af18e858b217ee5faa
TCLaurentiu <claudiu.tirisi@student.upt.ro>
2022-05-14 16:59:10 +0300

    BNNZ-56 Set up basic state for the video data, video file and thumbnail

--- bananza-frontend/pages/upload.js
+++ bananza-frontend/pages/upload.js
@@ -2,13 +2,35 @@ import PageHead from "/components/general/pageHead";
 import Nav from "/components/general/nav";
 import Input from "/components/forms/input";
 import styles from "../styles/upload.module.css";
+import React, { useEffect, useState } from "react";
 
 export default function Upload(props) {
+
+    let [videoData, setVideoData] = useState({
+        title:"",
+        description:"",
+    })
+
+    let updateVideoData = (e)=>{
+        if(e.target.type == "text"){
+            setVideoData({
+                ...videoData, [e.target.name]:e.target.value
+            })
+        }
+    }
+
+    let [video, setVideo] = useState(null);
+    let [thumbnail, setThumbnail] = useState(null);
+
+    let upload = (e)=>{
+        e.preventDefault();
+    }
+
   return (
     <>
       <PageHead title="Bananza - Edit Profile" />
       <Nav />
-      <form className={styles.uploadForm}>
+      <form onChange={updateVideoData} onSubmit={upload} className={styles.uploadForm}>
           <h2>Upload video</h2>
         <div className={styles.formWrap}>
           <div className="formFields">
@@ -27,13 +49,15 @@ export default function Upload(props) {
             <div className={styles.formColumn}>
               <label for="thumbnail">
                 Video thumbnail
-                <input name="thumbnail" id="thumbnail" type="file" />
+                <input name="thumbnail" 
+                    onChange={(e)=>{setThumbnail(e.target.files[0])}} id="thumbnail" type="file" />
               </label>
             </div>
             <div className={styles.formColumn}>
               <label for="video">
                 Video title
-                <input name="video" id="video" type="file" />
+                <input name="video"
+                    onChange={(e)=>{setVideo(e.target.files[0])}} id="video" type="file" />
               </label>
             </div>
           </div>
