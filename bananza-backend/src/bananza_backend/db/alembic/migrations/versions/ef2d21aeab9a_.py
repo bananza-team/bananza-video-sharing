f61f3d2896c72747c58b80012165edf7d6760bbb
serbanlex <serbanlex2001@yahoo.com>
2022-05-15 02:29:14 +0300

    BNNZ-81 Create database migration for defining Video details

---
+++ bananza-backend/src/bananza_backend/db/alembic/migrations/versions/ef2d21aeab9a_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: ef2d21aeab9a
+Revises: 3035416af83e, 3a5addc8fb64
+Create Date: 2022-05-15 00:32:41.901730
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ef2d21aeab9a'
+down_revision = ('3035416af83e', '3a5addc8fb64')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
