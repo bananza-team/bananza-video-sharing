97e365b78856789d13700afddcd20ca0db90877c
TCLaurentiu <claudiu.tirisi@student.upt.ro>
2022-05-15 21:33:40 +0300

    Set up basic search functionality using mock data

--- bananza-frontend/components/general/nav.js
+++ bananza-frontend/components/general/nav.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Router from "next/router";
 import React, { useState } from "react";
+import VideoList from "/components/videos/videolist";
 
 let logout = (event) => {
   event.preventDefault();
@@ -8,9 +9,12 @@ let logout = (event) => {
   Router.reload();
 };
 
+let matchedVideos = [];
+
 export default function Nav() {
 
   let [searching, setSearching] = useState(0);
+  let [renderedVideos, setRenderedVideos] = useState([]);
 
   let searchChange = (e)=>{
     if(!e.target.value.length){
@@ -18,7 +22,32 @@ export default function Nav() {
       return;
     }
     setSearching(1);
+
+    let keyword = e.target.value;
+    matchedVideos=[];
+    videos.forEach(video =>{
+      if(video.title.includes(keyword) || video.description.includes(keyword)) matchedVideos.push(video);
+    })
+    setRenderedVideos(matchedVideos);
   }
+
+  let videos = [
+    {
+      thumbnail: "test.png",
+      title: "lots of snow in the sea",
+      description: "description goes brr",
+      username: "Claudiu",
+      key: 0,
+    },
+    {
+      thumbnail: "test.png",
+      title: "the climate is changing",
+      description: "visit our lovely channel",
+      username: "Mircea",
+      key: 1,
+    },
+  ];
+  videos[2] = videos[3] = videos[4] = videos[0];
 
   return (
     <>
@@ -49,7 +78,7 @@ export default function Nav() {
     </nav>
     {!!searching && (
     <div className="searchContainer">
-      
+      <VideoList videos={renderedVideos} header="Search results" />
     </div>
     )}
     </>
