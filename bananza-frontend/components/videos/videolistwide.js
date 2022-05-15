367e98dc46f6fa0adcec3e9dc426ba4ea1f5c2ae
TCLaurentiu <claudiu.tirisi@student.upt.ro>
2022-05-15 22:38:25 +0300

    BNNZ-63 Create a video component that the video list component uses

---
+++ bananza-frontend/components/videos/videolistwide.js
@@ -0,0 +1,19 @@
+import VideoCardWide from "./videocardwide.js"
+import styles from "/styles/videolistwide.module.css"
+export default function VideoList(props){
+    return (
+        <div className={styles.videoList}>
+        <span className={styles.videoListHeader}>{props.header}</span>
+        <div className={styles.videoListContainer}>
+        { !!props.videos.length &&
+        props.videos.map((video, key) => (
+           <VideoCardWide video={video} key={key}/>
+        ))
+        }
+        { !props.videos.length && 
+            <span className={styles.notFound}>No videos found</span>
+        }
+        </div>
+        </div>
+    );
+}
