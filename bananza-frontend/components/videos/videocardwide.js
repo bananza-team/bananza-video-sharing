79eff08e8976218ce838507280b2ef63172c0a8c
TCLaurentiu <claudiu.tirisi@student.upt.ro>
2022-05-17 15:36:16 +0300

    BNNZ-163 Modify the videolistwide component to work with the data from the api

--- bananza-frontend/components/videos/videocardwide.js
+++ bananza-frontend/components/videos/videocardwide.js
@@ -2,13 +2,13 @@ import styles from "/styles/videocardwide.module.css"
 export default function VideoCard(props){
     return (
         <div className={styles.video}>
-        <img className={styles.thumbnail} src={props.video.thumbnail}/>
+        <img className={styles.thumbnail} src={`//localhost:8000${props.video.thumbnail_image_link}`}/>
         <div className={styles.videoData}>
         <span className={styles.titleDescription}>
         <span className={styles.videoTitle}>{props.video.title}</span>
         <span className={styles.videoDescription}>{props.video.description}</span>
         </span>
-        <span className={styles.postedBy}><span>posted by</span> {props.video.username}</span>
+        {props.username && <span className={styles.postedBy}><span>posted by</span> {props.username}</span>}
         </div>
         </div>
     )
