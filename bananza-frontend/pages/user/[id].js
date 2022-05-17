99f5be1d6a158741b0b61871f9328383be46235f
TCLaurentiu <claudiu.tirisi@student.upt.ro>
2022-05-17 15:19:00 +0300

    BNNZ-163 Fix the infinite loading of the user data by using useEffect

--- bananza-frontend/pages/user/[id].js
+++ bananza-frontend/pages/user/[id].js
@@ -4,36 +4,37 @@ import Input from "/components/forms/input";
 import styles from "/styles/user.module.css";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import Router from "next/router";
 import { NotificationManager } from "react-notifications";
 
-export default function User (){
-    let router = useRouter();
-    let { id } = router.query;
+export default function User() {
+  let router = useRouter();
+  let {id} = router.query;
 
-    if(!Number.isInteger(parseInt(id))){
-        router.push("/");
-        return;
-    }
+  useEffect(()=>{
+    fetch("//localhost:8000/user/"+id).then((response) =>
+      response.json().then((parsedJSON) => {
+          if(response.status == 200)
+          setUData(parsedJSON);
+          else router.push("/");
+      })
+    )}, []);
 
-    let [loading, setLoading] = useState(1);
-    let [userData, setUserData] = useState(null);
+let [fetching, setFetching] = useState(1);
+  let [uData, setUData] = useState({
+    username: "",
+    type: "",
+    description: "",
+    profile_picture_link: "",
+    cover_picture_link: "",
+    is_active: "",
+    videos: [],
+  });
 
-    fetch("//localhost:8000/user/"+id).then(response => response.json().then(parsedJSON=>{
-        loading = 0;
-        if(response.status == 200){
-            userData = parsedJSON;
-        } else {
-            setTimeout(()=>router.push("/"));
-            return;
-        }
-    }))
-
-    return (
-        <>
-            <PageHead title="Bananza - User Profile" />
-            <Nav />
-        </>
-    )
-
+  return (
+    <>
+      <PageHead title="Bananza - User Profile" />
+      <Nav />
+      {uData.username}
+    </>
+  );
 }
