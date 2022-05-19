f3d8d90d1ebb1003705cb77347eef10a4942a1ca
TCLaurentiu <claudiu.tirisi@student.upt.ro>
2022-05-19 23:47:21 +0300

    BNNZ-63 Use the Link component to redirect the user to the video or author profile page

--- bananza-frontend/components/videos/videocardwide.js
+++ bananza-frontend/components/videos/videocardwide.js
@@ -1,15 +1,16 @@
 import styles from "/styles/videocardwide.module.css"
+import Link from "next/link"
 export default function VideoCard(props){
     return (
         <div className={styles.video}>
         <img className={styles.thumbnail} src={`//localhost:8000${props.video.thumbnail_image_link}`}/>
         <div className={styles.videoData}>
         <span className={styles.titleDescription}>
-        <span className={styles.videoTitle}>{props.video.title}</span>
+        <span className={styles.videoTitle}><Link href={`/video/${props.video.id}`}>{props.video.title}</Link></span>
         <span className={styles.videoDescription}>{props.video.description}</span>
         </span>
         {/* <Link> can't be used because when {id} changes it doesn't trigger a page change, so the search box doesn't have the chance to disappear */}
-        {props.video.owner_id && props.displayPoster && <span className={styles.postedBy}><a href={`/user/${props.video.owner_id}`}>View poster profile</a></span>}
+        {props.video.owner_id && props.displayPoster && <span className={styles.postedBy}><Link href={`/user/${props.video.owner_id}`}>View poster profile</Link></span>}
         </div>
         </div>
     )
