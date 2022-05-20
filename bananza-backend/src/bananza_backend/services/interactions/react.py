7b7eb4fd3b0f764dced0ff5f8f500a99a7157295
serbanlex <serbanlex2001@yahoo.com>
2022-05-20 15:46:45 +0300

    BNNZ-119 Add repo logic for adding generic reaction, db operation

--- bananza-backend/src/bananza_backend/services/interactions/react.py
+++ bananza-backend/src/bananza_backend/services/interactions/react.py
@@ -23,3 +23,19 @@ class ReactionRepo:
             "dislikes": len(dislikes)
         }
 
+    def add_generic_reaction(self, reaction: ReactionCreate, user_id: int) -> Reaction:
+        new_reaction = ReactionModel(
+            video_id=reaction.video_id,
+            user_id=user_id,
+            state=reaction.state
+        )
+
+        try:
+            self.db.add(new_reaction)
+            self.db.commit()
+            self.db.refresh(new_reaction)
+            return new_reaction
+        except Exception as e:
+            logger.error(f"Couldn't add Reaction {new_reaction} to db. Reason: {e}")
+            raise e
+
