d4d0a749fc5e2fd491b380218711ddd9e340560a
serbanlex <serbanlex2001@yahoo.com>
2022-05-20 15:42:32 +0300

    BNNZ-173 Add endpoint for getting reactions (like/dislike) count on a video

--- bananza-backend/src/bananza_backend/api/interactions.py
+++ bananza-backend/src/bananza_backend/api/interactions.py
@@ -1,6 +1,7 @@
-from bananza_backend.models import Comment, CommentCreate
+from bananza_backend.models import Comment, CommentCreate, VideoReactionCount, ReactionCreate, Reaction
 from bananza_backend.db.sql_db import get_db
 from bananza_backend.services.interactions.comment import CommentRepo
+from bananza_backend.services.interactions.react import ReactionRepo
 from bananza_backend.services.authentication.handler import AuthHandler, oauth2_scheme
 
 from fastapi import APIRouter, Depends
@@ -9,8 +10,14 @@ from sqlalchemy.orm import Session
 router = APIRouter(prefix="/video/interact", tags=["Video interactions"])
 
 
-@router.post("/comment", summary="Get a list of all videos from db", response_model=Comment)
+@router.post("/comment", summary="Post a comment to a video", response_model=Comment)
 async def add_comment(comment: CommentCreate, db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
     user = AuthHandler(db).get_current_user_by_token(token)
     comment = CommentRepo(db).add(comment=comment, user_id=user.id)
     return comment
+
+
+@router.get("/reactions", summary="Get a count of likes and dislikes", response_model=VideoReactionCount)
+async def get_reactions(video_id: int, db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+    AuthHandler(db).get_current_user_by_token(token)
+    return ReactionRepo(db).get_count_on_video(video_id)
