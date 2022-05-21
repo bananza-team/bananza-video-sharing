b10ac45be2c3f1b27478aacb6e1ea42f9a4eaa13
TCLaurentiu <claudiu.tirisi@student.upt.ro>
2022-05-22 01:12:07 +0300

    BNNZ-88 Change the request method from PATCH to DELETE

--- bananza-frontend/pages/video/[id].js
+++ bananza-frontend/pages/video/[id].js
@@ -294,9 +294,9 @@ export default function Video(props) {
             label:"Yes",
             onClick:()=>{
               fetch("//localhost:8000/video/interact/comment/"+id, {
-                method:"PATCH",
-                header:{
-                  Authorize:"Bearer "+localStorage.token,
+                method:"DELETE",
+                headers:{
+                  Authorization:"Bearer "+localStorage.token,
                   'accept':'application/json'
                 }
               }).then(response=>response.json().then(parsedJSON=>{
