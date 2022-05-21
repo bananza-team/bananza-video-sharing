be2238e9f03bb709f7e48eaccb181565ecc37bb6
TCLaurentiu <claudiu.tirisi@student.upt.ro>
2022-05-21 22:49:42 +0300

    BNNZ-176 Add the same confirmation popup as in the video page to the video list component

--- bananza-frontend/components/videos/videocardwide.js
+++ bananza-frontend/components/videos/videocardwide.js
@@ -1,6 +1,27 @@
 import styles from "/styles/videocardwide.module.css"
 import Link from "next/link"
+import { confirmAlert } from 'react-confirm-alert'; // Import
+import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
 export default function VideoCard(props){
+
+    let deleteVideo = (e)=>{
+        confirmAlert({
+            title:"Confirm video deletion",
+            message:"Are you sure you want to delete the video?",
+            buttons:[
+                {
+                    label:"Yes",
+                    onClick:()=>{
+
+                    }
+                },
+                {
+                    label:"No",
+                }
+            ]
+        })
+    }
+
     return (
         <div className={styles.video}>
         <img className={styles.thumbnail} src={`//localhost:8000${props.video.thumbnail_image_link}`}/>
@@ -10,7 +31,7 @@ export default function VideoCard(props){
         <span className={styles.videoDescription}>{props.video.description}</span>
         </span>
         { (parseInt(props.user.id) == parseInt(props.video.owner_id) || props.user.type == "manager") && 
-            <span className={styles.deleteButton}>Delete</span>
+            <span onClick={deleteVideo} className={styles.deleteButton}>Delete</span>
         }
         {/* <Link> can't be used because when {id} changes it doesn't trigger a page change, so the search box doesn't have the chance to disappear */}
         {props.video.owner_id && props.displayPoster && <span className={styles.postedBy}><Link href={`/user/${props.video.owner_id}`}>View poster profile</Link></span>}
