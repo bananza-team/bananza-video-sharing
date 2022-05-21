87e0fa83c731b2dc82305e32375cde6e525fa5af
serbanlex <serbanlex2001@yahoo.com>
2022-05-21 16:41:46 +0300

    BNNZ-128 Add report adding logic to repo, with validation using the get method

--- bananza-backend/src/bananza_backend/services/interactions/report.py
+++ bananza-backend/src/bananza_backend/services/interactions/report.py
@@ -14,3 +14,29 @@ class ReportRepo:
         return self.db.query(ReportedVideoModel).filter(ReportedVideoModel.reporter_id.like(reporter_id),
                                                         ReportedVideoModel.video_id.like(video_id)).first()
 
+    def add_report(self, report: ReportedVideoCreate, reporter_id: int) -> ReportedVideo:
+        existing_report_of_user_on_this_video = self.get_by_user_and_video_id(reporter_id, report.video_id)
+
+        # TODO: validate that the reported video is not owned by the user himself (not prioritary/necesarry)
+        if existing_report_of_user_on_this_video and existing_report_of_user_on_this_video.answered is False:
+            raise HTTPException(status_code=403, detail=f"An unanswered report about this video has already been"
+                                                        f" submitted by user with id {reporter_id}")
+
+        return self.__add_report_in_db(reporter_id, report)
+
+    def __add_report_in_db(self, reporter_id: int, report: ReportedVideoCreate) -> ReportedVideo:
+        new_report = ReportedVideoModel(
+            answered=False,
+            reason=report.reason,
+            reporter_id=reporter_id,
+            video_id=report.video_id
+        )
+
+        try:
+            self.db.add(new_report)
+            self.db.commit()
+            self.db.refresh(new_report)
+            return new_report
+        except Exception as e:
+            logger.error(f"Couldn't add Reaction {new_report} to db. Reason: {e}")
+            raise e
