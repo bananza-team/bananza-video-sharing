4323c55cc484b19274e05d22b20e00987e279a87
TCLaurentiu <claudiu.tirisi@student.upt.ro>
2022-05-22 13:58:54 +0300

    BNNZ-112 Create a suspend button component

---
+++ bananza-frontend/components/others/suspend.js
@@ -0,0 +1,11 @@
+export default function Suspend(props){
+    
+    let buttonStyle = {
+        backgroundColor:"#1f1f1f",
+        marginRight:"10px",
+    }
+
+    return (
+        <button style={buttonStyle}>Suspend</button>
+    )
+}
