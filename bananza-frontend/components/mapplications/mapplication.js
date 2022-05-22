236e836a6ce6d547eba68e2d6f8012e5ec376129
TCLaurentiu <claudiu.tirisi@student.upt.ro>
2022-05-22 18:54:48 +0300

    BNNZ-117 Add event handlers to the accept and deny button;make the accept button display a confirmation popup

--- bananza-frontend/components/mapplications/mapplication.js
+++ bananza-frontend/components/mapplications/mapplication.js
@@ -1,6 +1,29 @@
 import styles from "/styles/application.module.css";
 import Link from "next/link";
+import { confirmAlert } from "react-confirm-alert";
 export default function Application(props){
+
+    let accept = ()=>{
+        confirmAlert({
+            description:"Are you sure you want to accept this application?",
+            title:"Confirm accepting this application",
+            buttons:[
+                {
+                    label:"Yes",
+                    onClick:()=>{
+
+                    }
+                }, {
+                    label:"No",
+                }
+            ]
+        })
+    }
+
+    let deny = ()=>{
+        
+    }
+
     return (
         <div className={styles.application}>
             <div className={styles.applicationUser}>
@@ -11,8 +34,8 @@ export default function Application(props){
             <div className={styles.applicationPhone}>{props.application.user.phone}</div>
             <div className={styles.applicationCV}><a href={`//localhost:8000${props.application.user.cv_link}`}><i class="fa-solid fa-file"></i></a></div>
             <div className={styles.applicationOptions}>
-                <i class="fa-solid fa-check"></i>
-                <i class="fa-solid fa-xmark"></i>
+                <i class="fa-solid fa-check" onClick={accept}></i>
+                <i class="fa-solid fa-xmark" onClick={deny}></i>
                 </div>
         </div>
     )
