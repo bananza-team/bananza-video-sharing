40c178b1357024347af125b42775da75fb14c339
TCLaurentiu <claudiu.tirisi@student.upt.ro>
2022-05-23 01:38:46 +0300

    BNNZ-180 Remove example tests, and add a test for logging out

--- bananza-frontend/tests/test.spec.js
+++ bananza-frontend/tests/test.spec.js
@@ -1,8 +1,12 @@
 // @ts-check
 let { test, expect } = require('@playwright/test');
 
-    test.beforeEach(async ({ page }) => {
-    await page.goto('localhost:3000');
+
+test.describe('test', async ()=>{
+let page;
+
+    test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
   });
 
   let user = {
@@ -10,12 +14,21 @@ let { test, expect } = require('@playwright/test');
       password:"parola",
   }
 
-  test.describe("Login", ()=>{
-      test("should allow me to log in", async ({page})=>{
+      test("should allow me to log in", async ()=>{
+        await page.goto("localhost:3000");
         await page.locator("#username").fill(user.username);
         await page.locator("#password").fill(user.password);
         await page.locator("button").press('Enter');
 
-        await expect(page.locator("nav")).toHaveCount(0);
-  })
-  })
+        await expect(page.locator("nav")).toHaveCount(1); // navigation is only present when the user is logged in
+      });
+
+      test("should go to the homepage when logging out", async ()=>{        
+      await page.goto("localhost:3000");
+      await page.locator("#username").fill(user.username);
+      await page.locator("#password").fill(user.password);
+      await page.locator("button").press('Enter');
+      await page.locator('text=Logout').click();
+      await expect(page.locator("nav")).toHaveCount(0);
+      })
+    })
