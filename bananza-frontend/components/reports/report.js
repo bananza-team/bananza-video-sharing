6b22c822316032d1cd103eb0050ca4d9f94fb1f2
TCLaurentiu <claudiu.tirisi@student.upt.ro>
2022-05-22 13:38:18 +0300

    BNNZ-108 Rewrite the report component to include all of the available information

--- bananza-frontend/components/reports/report.js
+++ bananza-frontend/components/reports/report.js
@@ -1,16 +1,24 @@
 import styles from "/styles/report.module.css"
+import Link from "next/link";
 export default function Report(props){
     return (
-    <div className={styles.report}>
-        <span className={styles.reportedVideo}>
-            {props.report.video.title}
-        </span>
-        <span className={styles.reportReason}>
-            {props.report.reportReason}
-        </span>
-        <span className={styles.reportedChannel}>
-            <span className="label">Channel</span>{props.report.video.channel}
-        </span>
-    </div>
+        <div className={styles.reportContainer}>
+            <div className={styles.reportedVideo}>
+                {!!props.report.video &&
+                <Link href={`/video/${props.report.video.id}`}>
+                    <a>{props.report.video.title}</a>
+                </Link>
+                }
+                {!props.report.video &&
+                    <span className={styles.videoRemoved}>Reported video has been removed</span>
+                }
+            </div>
+            <div className={styles.reportReason}>{props.report.reason}</div>
+            <div className={styles.reporter}>
+                <Link href={`/user/${props.report.reporter.id}`}>
+                    <a>{props.report.reporter.username}</a>
+                </Link>
+            </div>
+        </div>
     );
 }
