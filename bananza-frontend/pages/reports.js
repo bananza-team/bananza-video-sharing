5e8dada23712f9c97325ec5320dd29611eb34324
TCLaurentiu <claudiu.tirisi@student.upt.ro>
2022-05-22 13:13:49 +0300

    BNNZ-108 Fetch the reports and let the user know if an error occured

--- bananza-frontend/pages/reports.js
+++ bananza-frontend/pages/reports.js
@@ -1,6 +1,18 @@
-import {useRouter} from "next/router"
+import {useRouter} from "next/router";
+import { useEffect, useState} from "react";
+import { NotificationManager } from "react-notifications";
 export default function Reports(props){
     if(props.user == null || props.user.type == "creator"){
         useRouter().push("/");
     }
+
+    let [reports, setReports] = useState(null);
+    useEffect(()=>{
+        fetch("/video/interact/report").then(response=>response.json().then(parsedJSON=>{
+            if(response.status == 200){
+                setReports(parsedJSON);
+            } else NotificationManager.error("Reports could not be loaded");
+        }))
+    }, []);
+
 }
