14749e21e79135f5c2757b02bf21b492caf40e0a
TCLaurentiu <claudiu.tirisi@student.upt.ro>
2022-05-22 14:11:14 +0300

    BNNZ-179 Make a request to suspend the user and notify the user of the results

--- bananza-frontend/components/others/suspend.js
+++ bananza-frontend/components/others/suspend.js
@@ -1,4 +1,5 @@
 import { confirmAlert } from "react-confirm-alert";
+import { NotificationManager } from "react-notifications";
 
 export default function Suspend(props){
     
@@ -8,14 +9,25 @@ export default function Suspend(props){
     }
 
     let suspend = ()=>{
-        let id = props.user.id;
+        let id = props.user_id;
         confirmAlert({
             title:"Confirm user suspension",
             message:"Are you sure you want to suspend this user?",
             buttons:[{
                 label:"Yes",
                 onClick:()=>{
-
+                    fetch("//localhost:8000/user/suspend/"+id, {
+                        method:"PATCH",
+                        headers:{
+                            Authorization:"Bearer "+localStorage.token,
+                        }
+                    }).then(response=>response.json().then(parsedJSON=>{
+                        if(response.status == 200){
+                            NotificationManager.info("User succesfully suspended");
+                        } else if(response.status == 403)
+                            NotificationManager.error("Only an administrator can suspend a manager/admin");
+                        else NotificationManager.error("An error occured while trying to suspend the user");
+                    }))
                 }
             }, {
                 label:"No"
