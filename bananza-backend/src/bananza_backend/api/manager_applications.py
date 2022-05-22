b8b23c35bb11f8c495840dad20c9cdb660fe10f5
serbanlex <serbanlex2001@yahoo.com>
2022-05-22 17:22:47 +0300

    BNNZ-139 Remove unused endpoint for getting only one manager application

--- bananza-backend/src/bananza_backend/api/manager_applications.py
+++ bananza-backend/src/bananza_backend/api/manager_applications.py
@@ -1,5 +1,6 @@
 from bananza_backend.models import ManagerApplication
 from bananza_backend.db.sql_db import get_db
+from bananza_backend.services.authentication.handler import AuthHandler, oauth2_scheme
 from bananza_backend.services.manager_applications.repository import ManagerApplicationsRepo
 
 from fastapi import APIRouter, Depends, File, UploadFile
@@ -13,11 +14,4 @@ async def submit_user_application(user_id: str, cv_file: UploadFile = File(...),
     applications_repo = ManagerApplicationsRepo(db)
     new_application = await applications_repo.add(user_id=user_id, cv_file=cv_file)
 
-    return new_application
-
-
-@router.get("/{application_id}", summary="Get a user application from the db", response_model=ManagerApplication)
-async def get_user_application(application_id: str, db: Session = Depends(get_db)):
-    applications_repo = ManagerApplicationsRepo(db)
-    new_application = applications_repo.get_by_id(application_id=application_id)
     return new_application
